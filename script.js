d9aa959d508e4507f4670d42963e4a8e2a80ea92
Ragini Mishra <ragini.intern@amygo.app>
2023-07-09 10:49:00 +0530

    Basily-hiring assistance app

---
+++ script.js
@@ -0,0 +1,25 @@
+function navigateOnClick(id){
+    window.location.href=id;
+};
+function onLoginFormSubmit(){
+    alert("Logged in successfully!");
+    window.location.href='hireEmployees.html';
+};
+function onSearch() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+  
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  
+  }
